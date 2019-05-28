c0f90ceec6490d7cf226501743ced8f175334519
Daniel Smith <43457487+danielsmith1313@users.noreply.github.com>
2019-05-28 15:06:50 -0600

    Transferred png and ui files over to the folder
    Created a resources folder and transferred over the no_image.png and robot_ui.ui files

--- bin/__main__.py
+++ bin/__main__.py
@@ -42,8 +42,8 @@ class MyWindow(QtWidgets.QMainWindow):
         self.__longitudeList = []           #Contains the longitude of all inputs
         self.__altitudeList = []            #Contains the altitude of all the inputs
         self.__coordString = ""             #Contains the concantenated string to output to the application
-        self.__noImagePath = "../data/no_image.png"                 #Filename of the image for when there is no image
-        self.__guiPath = "../data/robot_ui.ui"
+        self.__noImagePath = "../resources/no_image.png"                 #Filename of the image for when there is no image
+        self.__guiPath = "../resources/robot_ui.ui"
         self.__noImagePixmap = QPixmap(self.__noImagePath)      #Convert the image to a QPixmap
         self.__controller = MoveRobot()                         #MoveRobot object
         self.__readwrite = JSONConverter("../data/longitude.txt", "../data/lattitude.txt","../data/altitude.txt")                      #JSONConverter object
