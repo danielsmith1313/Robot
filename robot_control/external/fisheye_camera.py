885980ce4948cdf61639a5a7f745afc8f64ded0e
Daniel Smith <43457487+danielsmith1313@users.noreply.github.com>
2019-05-29 10:46:57 -0600

    Changed file as it needs to be path

--- robot_control/external/fisheye_camera.py
+++ robot_control/external/fisheye_camera.py
@@ -8,8 +8,10 @@ import socket
 import json
 #INFO: Executable file that is remotely run to take a picture and send the picture back to the recieving end, 
 # in this case another raspberry pi
-recievingIP = ""
-filename = "run_import.py"
+
+#Declare variables
+recievingIP = ""                        #IP address of the raspberry pi
+filename = ""              #Path to the script
 #Take picture here and export it to the other pi as an array
 pic = []
 
