9d57ccd58ff77e6361d3740fb7f9fbc669deb42b
danielsmith1313 <danieljsmith1313@gmail.com>
2019-06-04 13:32:33 -0600

    Finished GPS reader so it works with the gps

--- robot_control/sensors_and_modules/gps_reader.py
+++ robot_control/sensors_and_modules/gps_reader.py
@@ -2,25 +2,59 @@
 #Author: Daniel Smith
 #Created: 5/23/2019
 #Last edited: 5/29/2019 by Daniel Smith
-#TODO: Transfer gps readings from move_robot to this
+#Code adapted from gps_simpletest.py: 
 
 class GPS:
     """
     This class is in charge of reading data and setting up the GPS.
     """
     def __init__():
-        pass
+        # for a computer, use the pyserial library for uart access
+        import serial
+        uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3000)
+        # Create a GPS module instance.
+        gps = adafruit_gps.GPS(uart, debug=False)
+        
+        # Initialize the GPS module by changing what data it sends and at what rate.
+        # These are NMEA extensions for PMTK_314_SET_NMEA_OUTPUT and
+        # PMTK_220_SET_NMEA_UPDATERATE but you can send anything from here to adjust
+        # the GPS module behavior:
+        #   https://cdn-shop.adafruit.com/datasheets/PMTK_A11.pdf
+
+        # Turn on the basic GGA and RMC info (what you typically want)
+        gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+        # Turn on the basic GGA and RMC info (what you typically want)
+        gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+        # Turn on just minimum info (RMC only, location):
+        #gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+        # Turn off everything:
+        #gps.send_command(b'PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+        # Turn on everything (not all of it is parsed!)
+        #gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
+        
+        # Set update rate to once a second (1hz) which is what you typically want.
+        gps.send_command(b'PMTK220,1000')
     #ClassMethod
-    def GetCurrentCoordinates(self, latorlong):
+    def GetHasFix(self):
+        if not gps.has_fix:
+            # Try again if we don't have a fix yet.
+            return False
+    def GetCurrentCoordinates(self, latOrLong):
         """
         Returns the most recent coordinates [lattitude, longitude, altitude]
         """
-        #TODO: Use gps to get current coordinates
+        
         #Return based on which option was chosen
-        if (latorlong == 0):
+        gps.update()
+        
+        
+        if (latOrLong == 0):
+            self.__currentLattitude = gps.latitude
             return self.__currentLattitude
-        elif(latorlong == 1):
+        elif(latOrLong == 1):
+            self.__currentLongitude = gps.longitude
             return self.__currentLongitude
+            
     def GetCurrentTrackAngle(self):
         """
         Returns the most recent track angle
