8ab7fcc3e37646a34874ce2522ca5bf7de8a138b
Daniel Smith <43457487+danielsmith1313@users.noreply.github.com>
2019-06-05 14:05:45 -0600

    Fixed gps

--- robot_control/sensors_and_modules/gps_reader.py
+++ robot_control/sensors_and_modules/gps_reader.py
@@ -13,12 +13,25 @@ class GPS:
     """
 
     def __init__(self):
+    # ClassMethod
+        pass
+
+    def GetHasFix(self):
+        if not self.__gps.has_fix:
+            # Try again if we don't have a fix yet.
+            return False
+
+    def GetCurrentCoordinates(self, option):
+        """
+        Returns the most recent coordinates [lattitude, longitude, altitude]
+        """
+        
         # for a computer, use the pyserial library for uart access
         import serial
         self.__uart = serial.Serial(
             "/dev/ttyUSB0", baudrate=9600, timeout=3000)
         # Create a GPS module instance.
-        self.__gps = adafruit_gps.GPS(self.__uart, debug=False)
+        self.gps = adafruit_gps.GPS(self.__uart, debug=False)
 
         # Initialize the GPS module by changing what data it sends and at what rate.
         # These are NMEA extensions for PMTK_314_SET_NMEA_OUTPUT and
@@ -27,10 +40,10 @@ class GPS:
         #   https://cdn-shop.adafruit.com/datasheets/PMTK_A11.pdf
 
         # Turn on the basic GGA and RMC info (what you typically want)
-        self.__gps.send_command(
+        self.gps.send_command(
             b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
         # Turn on the basic GGA and RMC info (what you typically want)
-        self.__gps.send_command(
+        self.gps.send_command(
             b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
         # Turn on just minimum info (RMC only, location):
         # gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
@@ -40,56 +53,34 @@ class GPS:
         # gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
 
         # Set update rate to once a second (1hz) which is what you typically want.
-        self.__gps.send_command(b'PMTK220,1000')
-    # ClassMethod
-
-    def GetHasFix(self):
-        if not self.__gps.has_fix:
-            # Try again if we don't have a fix yet.
-            return False
-
-    def GetCurrentCoordinates(self, latOrLong):
-        """
-        Returns the most recent coordinates [lattitude, longitude, altitude]
-        """
-
-        running = True
+        self.gps.send_command(b'PMTK220,1000')
+        # Main loop runs forever printing the location, etc. every second.
         last_print = time.monotonic()
-        while running == True:
-            # Return based on which option was chosen
-            self.__gps.update()
+        while True:
+            # Make sure to call gps.update() every loop iteration and at least twice
+            # as fast as data comes from the GPS unit (usually every second).
+            # This returns a bool that's true if it parsed new data (you can ignore it
+            # though if you don't care and instead look at the has_fix property).
+            self.gps.update()
+            # Every second print out current location details if there's a fix.
             current = time.monotonic()
             if current - last_print >= 1.0:
                 last_print = current
-                if not self.__gps.has_fix:
-                    print('waiting for fix')
+                if not self.gps.has_fix:
+                    # Try again if we don't have a fix yet.
+                    print('Waiting for fix...')
                     continue
 
-                if (latOrLong == 0 and self.__gps.latitude is not None):
-                    self.__currentLattitude = self.__gps.latitude
-                    return self.__currentLattitude
-                    running = False
-                elif(latOrLong == 1 and self.__gps.longitude is not None):
-                    self.__currentLongitude = self.__gps.longitude
-                    return self.__currentLongitude
-                    running = False
+                if option == 0:
+                    self.__currentLattitude =  self.gps.latitude
+                elif option == 1:
+                    self.__currentAltitude = gps.longitude
+                # Some attributes beyond latitude, longitude and timestamp are optional
+                # and might not be present.  Check if they're None before trying to use!
+                elif option == 2:
+                    if gps.track_angle_deg is not None:
+                        self.__trackAngle = gps.track_angle_deg
+                
 
     def GetCurrentTrackAngle(self):
-        """
-        Returns the most recent track angle
-        """
-        # TODO: use gps coordinates to get actual tracking
-
-        running = True
-        last_print = time.monotonic()
-        while running == True:
-            # Return based on which option was chosen
-            self.__gps.update()
-            current = time.monotonic()
-            if current - last_print >= 1.0:
-                if not self.__gps.has_fix:
-                    print('waiting for fix')
-                    continue
-                if self.__gps.track_angle_deg is not None:
-                    self.__trackAngle = self.__gps.track_angle_deg
-                    return self.__trackAngle
+        return GetCurrentCoordinates(3)
