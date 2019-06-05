bfedb0ff9aee90a6d2807318ee4b87da2fd73cf5
Daniel Smith <43457487+danielsmith1313@users.noreply.github.com>
2019-06-05 13:17:05 -0600

    formatting changes

--- robot_control/sensors_and_modules/fisheye_camera.py
+++ robot_control/sensors_and_modules/fisheye_camera.py
@@ -1,51 +1,54 @@
 #Filename: fisheye_camera.py
-#Author: Daniel Smith
+# Author: Daniel Smith
 #Created: 5/23/2019
-#Last edited: 6/4/2019 by Daniel Smith
+# Last edited: 6/4/2019 by Daniel Smith
 
 from picamera import PiCamera
-#import 
+# import
 import piexif
 from PIL import Image
 from time import sleep
 import uuid
 import datetime
+
+
 class FisheyeCamera():
     """
     This static class is in charge of getting data from the front facing camera
     """
-    
+
     def __init__(self):
         pass
 
     @classmethod
-    def TakePicture(self,longitude,lattitude):
+    def TakePicture(self, longitude, lattitude):
         """
         Sends a signal to recieve data from the front facing camera
         """
-        #Create object to get date and time
+        # Create object to get date and time
         now = datetime.datetime.now()
-        
-        #Generate a unique filename
+
+        # Generate a unique filename
         self.uniqueFilename = str(uuid.uuid4())
-        self.path = str("../../data/pictures/fisheye/",self.uniqueFilename,sep='')
+        self.path = str("../../data/pictures/fisheye/",
+                        self.uniqueFilename, sep='')
         self.path += ".jpeg"
         camera.start_preview()
         sleep(1)
-        #Capture and save the image
+        # Capture and save the image
         camera.capture(self.path)
-        img = Image.open(fname)
+        img = Image.open(self.path)
         exif_dict = piexif.load(img.info['exif'])
         exif_dict['GPS'][piexif.GPSIFD.GPSLongitude] = (longitude, 1)
-        exif_dict['GPS'][piexif.GPSIFD.GPSLattitude] = (lattitude,1)
-        #Get the current date and time
-        currentDateAndTime = str(now.year, ":", now.month, ":", now.day, " ", now.hour, ":", now.minute, ":", now.second, sep='').decode(encoding = "utf-8")
-        exif_dict['GPS'][piexif.GPSIFD.GPSDateStamp] = (currentDateAndTime) 
-        
+        exif_dict['GPS'][piexif.GPSIFD.GPSLattitude] = (lattitude, 1)
+        # Get the current date and time
+        currentDateAndTime = str(now.year, ":", now.month, ":", now.day, " ", now.hour,
+                                 ":", now.minute, ":", now.second, sep='').decode(encoding="utf-8")
+        exif_dict['GPS'][piexif.GPSIFD.GPSDateStamp] = (currentDateAndTime)
 
-        
         exif_bytes = piexif.dump(exif_dict)
         img.save(self.path)
+
     @classmethod
     def ExportImage(self):
         """
--- robot_control/sensors_and_modules/gps_reader.py
+++ robot_control/sensors_and_modules/gps_reader.py
@@ -1,21 +1,25 @@
 #Filename: gps_reader.py
-#Author: Daniel Smith
+# Author: Daniel Smith
 #Created: 5/23/2019
-#Last edited: 5/29/2019 by Daniel Smith
-#Code adapted from gps_simpletest.py: 
+# Last edited: 5/29/2019 by Daniel Smith
+# Code adapted from gps_simpletest.py:
 import adafruit_gps
 import time
+
+
 class GPS:
     """
     This class is in charge of reading data and setting up the GPS.
     """
+
     def __init__(self):
         # for a computer, use the pyserial library for uart access
         import serial
-        self.__uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3000)
+        self.__uart = serial.Serial(
+            "/dev/ttyUSB0", baudrate=9600, timeout=3000)
         # Create a GPS module instance.
         self.__gps = adafruit_gps.GPS(self.__uart, debug=False)
-        
+
         # Initialize the GPS module by changing what data it sends and at what rate.
         # These are NMEA extensions for PMTK_314_SET_NMEA_OUTPUT and
         # PMTK_220_SET_NMEA_UPDATERATE but you can send anything from here to adjust
@@ -23,54 +27,57 @@ class GPS:
         #   https://cdn-shop.adafruit.com/datasheets/PMTK_A11.pdf
 
         # Turn on the basic GGA and RMC info (what you typically want)
-        self.__gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+        self.__gps.send_command(
+            b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
         # Turn on the basic GGA and RMC info (what you typically want)
-        self.__gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+        self.__gps.send_command(
+            b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
         # Turn on just minimum info (RMC only, location):
-        #gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+        # gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
         # Turn off everything:
-        #gps.send_command(b'PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+        # gps.send_command(b'PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
         # Turn on everything (not all of it is parsed!)
-        #gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
-        
+        # gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
+
         # Set update rate to once a second (1hz) which is what you typically want.
         self.__gps.send_command(b'PMTK220,1000')
-    #ClassMethod
+    # ClassMethod
+
     def GetHasFix(self):
         if not gps.has_fix:
             # Try again if we don't have a fix yet.
             return False
+
     def GetCurrentCoordinates(self, latOrLong):
         """
         Returns the most recent coordinates [lattitude, longitude, altitude]
         """
-        
 
-	running = True
-	last_print = time.monotonic()
-	while running == True:
-        #Return based on which option was chosen
+        running = True
+        last_print = time.monotonic()
+        while running == True:
+            # Return based on which option was chosen
             self.__gps.update()
             current = time.monotonic()
-            if current - last_print >= 1.0
-		if not gps.has_fix:
-		    print('waiting for fix')
-		    continue
-            
+            if current - last_print >= 1.0:
+                if not self.__gps.has_fix:
+                    print('waiting for fix')
+                    continue
+
                 if (latOrLong == 0 and self.__gps.latitude is not None):
                     self.__currentLattitude = self.__gps.latitude
                     return self.__currentLattitude
-		    running = False
+                    running = False
                 elif(latOrLong == 1 and self.__gps.longitude is not None):
                     self.__currentLongitude = self.__gps.longitude
                     return self.__currentLongitude
-		    running = False
-            
+                    running = False
+
     def GetCurrentTrackAngle(self):
         """
         Returns the most recent track angle
         """
-        #TODO: use gps coordinates to get actual tracking
-        if gps.track_angle_deg is not None:
+        # TODO: use gps coordinates to get actual tracking
+        if self.__gps.track_angle_deg is not None:
             self.__trackAngle = self.__gps.track_angle_deg
         return self.__trackAngle
