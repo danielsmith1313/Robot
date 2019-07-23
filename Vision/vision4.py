a80744c1cc1d13676f150ea709934adb5e3d3c09
Daniel Smith <43457487+danielsmith1313@users.noreply.github.com>
2019-07-23 14:51:03 -0600

    Added wait key and close

--- Vision/vision4.py
+++ Vision/vision4.py
@@ -142,5 +142,6 @@ while True:
     distance_text = cv2.putText(orig,str(dist),(10, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
     cv2.imshow("image", orig)
     #cv2.imshow("mask", mask)
-    cv2.waitKey(1)
     
+    cv2.waitKey(1000)
+    cv2.destroyAllWindows()
