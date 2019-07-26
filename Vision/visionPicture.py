b6ace85a82ea5b6d7c2f429a7d047164c8c24f47
Daniel Smith <43457487+danielsmith1313@users.noreply.github.com>
2019-07-26 11:17:20 -0600

    D

--- Vision/visionPicture.py
+++ Vision/visionPicture.py
@@ -45,7 +45,7 @@ while True:
     hsvleaf = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     #Get the green filter for output
     
-    maskleaf = cv2.inRange(hsvleaf, GREENMASK)
+    maskleaf = cv2.inRange(hsvleaf, (36, 25, 25), (75, 255,255))
     imask = maskleaf>0
     ## slice the green, replacing other colors with black
     imaskleaf = maskleaf>0
@@ -64,7 +64,7 @@ while True:
 
     ## mask of green (36,25,25) ~ (86, 255,255)
     # mask = cv2.inRange(hsv, (36, 25, 25), (86, 255,255))
-    mask = cv2.inRange(hsv, GREENMASK)
+    mask = cv2.inRange(hsv, (36, 25, 25), (75, 255,255))
 
     ## slice the green, replacing other colors with black
     imask = mask>0
@@ -77,7 +77,7 @@ while True:
     kernel = np.ones((10,10),np.float32)/100
     dst = cv2.filter2D(green,-1,kernel)
     #Find the textures that are green and replace the green with pink
-    mask = cv2.inRange(dst, GREENMASK)
+    mask = cv2.inRange(dst, (36, 25, 25), (75, 255,255))
     imask = mask>0
     pink = np.zeros_like(green, np.uint8)
     pink[imask] = (127,0,255)
