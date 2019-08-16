71f92714dceb3ee8ccae64c3aa4ecea4afafd283
Daniel Smith <43457487+danielsmith1313@users.noreply.github.com>
2019-08-16 15:07:45 -0600

    Added a dropbox uploading program
    TODO: Implement into a class structure, implement into the raspberry pi

---
+++ bin/dropbox.py
@@ -0,0 +1,19 @@
+import dropbox, sys, os
+
+dbx = dropbox.Dropbox('zKAyq68NMfAAAAAAAAAALCvDPQzGNLaxSsZAWYEURF_lOxfoY3hdFkNrRlox41-n')
+rootdir = './resources' 
+
+print ("Attempting to upload...")
+# walk return first the current folder that it walk, then tuples of dirs and files not "subdir, dirs, files"
+for dir, dirs, files in os.walk(rootdir):
+    for file in files:
+        try:
+            file_path = os.path.join(dir, file)
+            dest_path = os.path.join('/test', file)
+            print('Uploading '+file_path+ ' to ' + dest_path)
+            with open(file_path) as f:
+                dbx.files_upload(f, dest_path, mute=True)
+        except Exception as err:
+            print("Failed to upload "+ file + " " + err)
+
+print("Finished upload.")
