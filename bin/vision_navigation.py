6a5dd436b484f835ec42e0700617bf4527271bd3
Daniel Smith <43457487+danielsmith1313@users.noreply.github.com>
2019-08-21 12:26:10 -0600

    cfg changed

--- bin/vision_navigation.py
+++ bin/vision_navigation.py
@@ -27,7 +27,7 @@ class VisionNavigation:
     def navigate(self):
         BLUR = 200
         #Calibration tool to make the robot turn left (negative) or right (positive)
-        OFFSET = -50
+        OFFSET = 175
         PIXEL_PERCENT_FOR_BROWN = .5
         #The number of rows the robot goes through
         NUMOFROWS = 10
