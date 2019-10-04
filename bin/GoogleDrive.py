dfe0ec18de59ab0d6e25f32f0e58d611d15e4ef0
Daniel Smith <43457487+danielsmith1313@users.noreply.github.com>
2019-10-04 13:27:22 -0600

    Added automatic validation
    The Google Drive program now can have a client_secrets.json file from a google drive developer account and will remember those settings.

--- bin/GoogleDrive.py
+++ bin/GoogleDrive.py
@@ -1,16 +1,39 @@
 import os
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-#1st authentification
+import datetime
+
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # Creates local webserver and auto handles 
+# Try to load saved client credentials
+gauth.LoadCredentialsFile("mycreds.txt")
+if gauth.credentials is None:
+    # Authenticate if they're not there
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    # Refresh them if expired
+    gauth.Refresh()
+else:
+    # Initialize the saved creds
+    gauth.Authorize()
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("mycreds.txt")
 #authentication.
 drive = GoogleDrive(gauth)
+
+#Create folder
+fisheyeFolderMetadata = {'title':'Fisheye '+ str(datetime.datetime.now()), 'mimeType' : 'application/vnd.google-apps.folder'}
+fisheyeFolder = drive.CreateFile(fisheyeFolderMetadata)
+fisheyeFolder.Upload()
+
+#Find folder info
+fisheyeFolderTitle = fisheyeFolder['title']
+fisheyeFolderID = fisheyeFolder['id']
+
 
 rootdir = 'C:/Users/dsmith129/Documents/GitHub/Robot/bin/data'
 for subdir,dirs,files in os.walk(rootdir):
     for file in files:
         fileToUpload = os.path.join(subdir, file)
-        newFile = drive.CreateFile({'RowBot': os.path.basename(fileToUpload)})
+        newFile = drive.CreateFile({'title':os.path.basename(fileToUpload), 'parents':[{'kind': 'drive#fileLink', 'id': fisheyeFolderID}]})
         newFile.SetContentFile(fileToUpload)
         newFile.Upload()
